52122ce21752249a72d1927f6d9f6bb2c0bf4c2c
aniket prj <aniketstudy20503@gmail.com>
2024-04-03 03:02:36 +0530

    About Section Created

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,12 +1,11 @@
-'use client'
-import React from 'react'
-import { ThemeProvider } from 'next-themes'
+"use client";
+import React from "react";
+import { ThemeProvider } from "next-themes";
 
-export default function Providers({children}) {
+export default function Providers({ children }) {
   return (
-    <ThemeProvider defaultTheme='system' attribute='class'>
-    <div className="">{children}</div>
-      
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen select-none transition-colors duration-300">{children}</div>
     </ThemeProvider>
-  )
+  );
 }
