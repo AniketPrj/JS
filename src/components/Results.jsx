2cb9c92d13e818d3682ac7cccd95f3de829f85cb
aniket prj <aniketstudy20503@gmail.com>
2024-04-08 01:43:42 +0530

    same last

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const Results = ({ results }) => {
   return (
-    <div className="max-w-6xl p-3 mx-auto py-4 sm:grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div className="max-w-6xl mx-auto py-4 sm:grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {results.map((result) => (
         <Card key={result.id} result={result} />
       ))}
